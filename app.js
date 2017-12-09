f55f75a6683e7df2cdb92d2cbee792a22885bbd9
郭伟杰 <529844698@qq.com>
2017-12-09 15:03:08 +0800

    初始化项目

---
+++ app.js
@@ -0,0 +1,31 @@
+//app.js
+var Bmob = require('utils/bmob.js')
+Bmob.initialize("3aefa2db08b19ecf61a8771d43afc413", "18f4c89e8d2094454a08e8c99412c714");
+App({
+  onLaunch: function() {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },
+
+  getUserInfo: function(cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.getUserInfo({
+        withCredentials: false,
+        success: function(res) {
+          that.globalData.userInfo = res.userInfo
+          typeof cb == "function" && cb(that.globalData.userInfo)
+        }
+      })
+    }
+  },
+
+  globalData: {
+    userInfo: null
+  }
+})
