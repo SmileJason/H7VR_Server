20d3ddc2577eb4f40886902e86d86582d55bc94a
郭伟杰 <529844698@qq.com>
2017-12-18 13:58:05 +0800

    SSL验证

--- H7VR/urls.py
+++ H7VR/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
         }),
+    url(r'^.well-known/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
 ]
